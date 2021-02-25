74ae30a838adb638179025c132c6879f77a4757e
ylm-sigmund <1024634983@qq.com>
2021-02-25 23:23:43 +0800

    完善MAT内存分析

--- jdk8features/src/test/java/com/diy/sigmund/testsome/HprofTest.java
+++ jdk8features/src/test/java/com/diy/sigmund/testsome/HprofTest.java
@@ -1,6 +1,9 @@
 package com.diy.sigmund.testsome;
 
+import com.diy.sigmund.entity.User;
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -22,12 +25,25 @@ public class HprofTest {
      * Dumping heap to F:\project\knight\github\testsome\test.hprof ...
      * Heap dump file created
      *
+     * VM options:-Xmx10m -XX:+HeapDumpOnOutOfMemoryError
+     *
      * @param args args
      * @throws InterruptedException InterruptedException
      */
     public static void main(String[] args) throws InterruptedException {
-        List<Integer> ids = new ArrayList<>();
+//        List<Integer> ids = new ArrayList<>();
+//        for (int i = 0; i < 1230000; i++) {
+//            ids.add(123456789);
+//        }
 
+        List<User> users =new ArrayList<>();
+        for (int i = 0; i < 12340000; i++) {
+            final User user = new User();
+            user.setId(i);
+            user.setName(i+"");
+            user.setDate(new Date());
+            users.add(user);
+        }
         TimeUnit.MINUTES.sleep(10);
     }
 }
